63bb59d6745c6b124f4d6e12b8e9d94b181ee40b
Brian Manning <brian@xaoc.org>
2013-07-07 22:21:43 -0700

    app: added Config object with get/set/defined methods
    - Removed TODOs and added them as issues on GitHub
    - Changed a lot of 'defined' calls to be $obj->defined(q(attribute))

--- bin/idgames_mirror.pl
+++ bin/idgames_mirror.pl
@@ -23,36 +23,6 @@ Create or update a mirror of the C<idgames> archive on the local host.
 
 =cut
 
-
-# TODO
-# - always pull the ls-laR.gz file from the primary mirror, unless a custom
-#   --url is specified
-# - don't sync files in /incoming, don't list them as well unless
-#   --incoming is turned on(?) (don't show in output reports, as well as
-#   don't try downloads from incoming unless --incoming is used)
-# - Download retries to the master mirror (fu-berlin) don't display the
-# download location if the download was successful
-# - add a 'script' report format, for use by other scripts, up to and
-# including a GTK front end
-# - add deleting of files off of the local filesystem using the --delete
-# switch; this would be used mostly for the /newstuff directory
-# - add Devel::Size coverage
-# - add "strict" checking; differing dates and permissions will also fail a
-# test; it's actually hard to check for different size directories, as the
-# directory size between different filesystems (archive filesystem and local
-# filesystem) will only be the same size when both machines have the same OS,
-# OS Version and disk sizes
-# - add transaction logging; keep a record of all changes made to the local
-# system; it won't be possible to back those changes out, but at least you'll
-# have a log of what happened.
-# - work more with Log::Log4perl so you can narrow down debugging output as
-# needed
-# - add a --pause/--wait switch to add time in between downloads
-
-# Tests:
-# - creating a directory when a file with the same name lives on the
-# filesystem
-#
 # ! = script defaults
 #
 # script reports:
@@ -84,7 +54,6 @@ Create or update a mirror of the C<idgames> archive on the local host.
 # - Reporter - writes reports based on the type of report specified by the
 # user
 
-
 our @options = (
     # help/verbosity options
     q(help|h),
@@ -108,6 +77,8 @@ our @options = (
     q(dotfiles), # don't show dotfiles in reports - .filename
     q(headers), # show directory headers and blocks used
     q(tempdir=s), # temporary directory to use for tempfiles
+    q(update-ls-lar), # update the ls-laR.gz file, then exit
+    q(use-local-ls-lar), # use the local copy of ls-laR.gz file to check files
     # combination options
     q(size-local|local-size|sl), # show size mismatches, missing local files
     q(size-same|ss), # show size mismatches and missing local files
@@ -219,23 +190,51 @@ errors were encountered.
 
 =head2 idGames::Mirror::Config
 
-Handle/manage script options using L<Getopt::Long>.
+Configure/manage script options using L<Getopt::Long>.
+
+=head3 Methods
+
+=over
 
 =cut
 
 package idGames::Mirror::Config;
 
-    # force writes in output to STDOUT
-    $| = 1;
-    my %opts;
-    my $p = Getopt::Long::Parser->new();
-    $p->getoptions(
-        \%opts,
-    );
+sub new {
+    my $class = shift;
 
-    if ( defined $opts{help} ) {
-        pod2usage( { -verbose => 1, -exitval => 0, -input => __FILE__ } );
+    my $self = bless ({}, $class);
+
+    # script arguments
+    my %args;
+
+    # parse the command line arguments (if any)
+    my $parser = Getopt::Long::Parser->new();
+
+    # pass in a reference to the args hash as the first argument
+    $parser->getoptions( \%args, @options );
+
+    # assign the args hash to this object so it can be reused later on
+    $self->{_args} = \%args;
+
+    # dump and bail if we get called with --help
+    if ( $self->get(q(help)) ) { pod2usage(-exitstatus => 1); }
+
+    # dump and bail if we get called with --examples
+    if ( $self->get(q(examples)) ) {
+        $self->show_examples();
+        exit 0;
     }
+
+    # return this object to the caller
+    return $self;
+}
+
+=item show_examples
+
+Show examples of script usage.
+
+=cut
 
 sub show_examples {
 
@@ -270,6 +269,86 @@ print <<EXAMPLES;
 EXAMPLES
 
 } # sub examples
+
+=item get($key)
+
+Returns the scalar value of the key passed in as C<key>, or C<undef> if the
+key does not exist in the L<JenkBuilder::Config> object.
+
+=cut
+
+sub get {
+    my $self = shift;
+    my $key = shift;
+    # turn the args reference back into a hash with a copy
+    my %args = %{$self->{_args}};
+
+    if ( exists $args{$key} ) { return $args{$key}; }
+    return undef;
+}
+
+=item set( key => $value )
+
+Sets in the L<JenkBuilder::Config> object the key/value pair passed in
+as arguments.  Returns the old value if the key already existed in the
+L<JenkBuilder::Config> object, or C<undef> otherwise.
+
+=cut
+
+sub set {
+    my $self = shift;
+    my $key = shift;
+    my $value = shift;
+    # turn the args reference back into a hash with a copy
+    my %args = %{$self->{_args}};
+
+    if ( exists $args{$key} ) {
+        my $oldvalue   = $args{$key};
+        $args{$key}    = $value;
+        $self->{_args} = \%args;
+        return $oldvalue;
+    } else {
+        $args{$key}    = $value;
+        $self->{_args} = \%args;
+    } # if ( exists $args{$key} )
+    return undef;
+}
+
+=item get_args( )
+
+Returns a hash containing the parsed script arguments.
+
+=cut
+
+sub get_args {
+    my $self = shift;
+    # hash-ify the return arguments
+    return %{$self->{_args}};
+}
+
+=item defined($key)
+
+Returns "true" (C<1>) if the value for the key passed in as C<key> is
+C<defined>, and "false" (C<0>) if the value is undefined, or the key doesn't
+exist.
+
+=cut
+
+sub defined {
+    my $self = shift;
+    my $key = shift;
+    # turn the args reference back into a hash with a copy
+    my %args = %{$self->{_args}};
+
+    if ( exists $args{$key} ) {
+        if ( defined $args{$key} ) {
+            return 1;
+        }
+    }
+    return 0;
+}
+
+=back
 
 =head2 Role::Dir::Attribs
 
@@ -1741,17 +1820,13 @@ errors were encountered.
 
     # force writes in output to STDOUT
     $| = 1;
-    my %opts;
-    my $p = Getopt::Long::Parser->new();
-    $p->getoptions(
-        \%opts,
-    );
+    my $cfg = idGames::Mirror::Config->new();
 
-    if ( defined $opts{help} ) {
+    if ( defined $cfg->get(q(help)) ) {
         pod2usage( { -verbose => 1, -exitval => 0, -input => __FILE__ } );
     }
 
-    if ( defined $opts{examples} ) {
+    if ( defined $cfg->get(q(examples)) ) {
         show_examples();
         exit 0;
     }
@@ -1760,28 +1835,28 @@ errors were encountered.
     my $parent = q();
     # flag set whenever we're parsing files in/beneath the /incoming directory
     my $incoming_dir_flag = 0;
-    #if ( defined $opts{q(incoming)} ) {
+    #if ( defined $cfg->get(q(incoming)} ) {
     #    $incoming_dir_flag = 1;
     #} else {
     #    $incoming_dir_flag = 0;
     #}
     # default log level
     my $log4perl_conf = qq(log4perl.rootLogger = WARN, Screen\n);
-    if ( defined $opts{debug} && ! defined $opts{verbose} ) {
+    if ( $cfg->defined(q(debug)) && ! $cfg->defined(q(verbose)) ) {
         $log4perl_conf = qq(log4perl.rootLogger = DEBUG, Screen\n);
-    } elsif ( defined $opts{verbose} && ! defined $opts{debug} ) {
+    } elsif ( $cfg->defined(q(verbose)) && ! $cfg->defined(q(debug)) ) {
         $log4perl_conf = qq(log4perl.rootLogger = INFO, Screen\n);
-    } elsif ( defined $opts{verbose} && defined $opts{debug} ) {
+    } elsif ( $cfg->defined(q(verbose)) && $cfg->defined(q(debug)) ) {
         die(q(Script called with --debug and --verbose; choose one!));
     }
 
     # color log output
-    if ( defined $opts{color} ) {
-        $log4perl_conf .= q(log4perl.appender.Screen )
-            . qq(= Log::Log4perl::Appender::Screen\n);
-    } else {
+    if ( -t STDOUT || $cfg->defined(q(colorize))) {
         $log4perl_conf .= q(log4perl.appender.Screen )
             . qq(= Log::Log4perl::Appender::ScreenColoredLevels\n);
+    } else {
+        $log4perl_conf .= q(log4perl.appender.Screen )
+            . qq(= Log::Log4perl::Appender::Screen\n);
     }
 
     # the rest of the log4perl setup
@@ -1796,35 +1871,36 @@ errors were encountered.
     $log->debug(__FILE__ . qq(: script start; ) . time2str(q(%C), time));
 
     my @exclude_urls;
-    if ( defined $opts{exclude} ) {
-        @exclude_urls = @{$opts{exclude}};
+    if ( $cfg->defined(q(exclude)) ) {
+        @exclude_urls = @{$cfg->get(q(exclude))};
     }
 
     # set a temporary directory; this directory is used when downloading
     # files, LWP::Wrapper downloads to the file directly instead of
     # downloading to an object in memory
-    if ( ! defined $opts{tempdir} ) {
+    if ( ! $cfg->defined(q(tempdir)) ) {
         if ( defined $ENV{TEMP} ) {
             # FIXME need to set taint mode, and untaint the environment
             # variables used below
-            $opts{tempdir} = $ENV{TEMP};
-            $log->debug(__FILE__ . q(: setting tempdir to ) . $opts{tempdir});
+            $cfg->get(tempdir} = $ENV{TEMP};
+            $log->debug(__FILE__ . q(: setting tempdir to )
+                . $cfg->get(q(tempdir)) );
         } elsif ( defined $ENV{TMP} ) {
-            $opts{tempdir} = $ENV{TMP};
+            $cfg->get(tempdir} = $ENV{TMP};
         } elsif ( defined $ENV{TMPDIR} ) {
-            $opts{tempdir} = $ENV{TMPDIR};
+            $cfg->get(tempdir} = $ENV{TMPDIR};
         } else {
             # FIXME this only works on UNIX-y platforms
-            $opts{tempdir} = q(/tmp);
+            $cfg->get(tempdir} = q(/tmp);
         }
     }
     my $lwp = LWP::Wrapper->new(
-        base_url        => $opts{url},
+        base_url        => $cfg->get(q(url)),
         exclude_urls    => \@exclude_urls,
-        tempdir         => $opts{tempdir},
+        tempdir         => $cfg->get(q(tempdir)),
     );
 
-    if ( defined $opts{q(show-mirrors)} ) {
+    if ( $cfg->defined(q(show-mirrors)) ) {
         print qq(Current mirror URLs:\n);
         foreach my $mirror ( $lwp->get_mirror_list() ) {
             print qq(- $mirror\n);
@@ -1833,11 +1909,11 @@ errors were encountered.
     }
 
     $log->logdie(q(Must specify path directory with --path))
-        unless ( defined $opts{path} );
+        unless ( $cfg->defined(q(path)) );
 
     ### REPORT TYPES
-    if ( defined $opts{type} ) {
-        my @reports = @{$opts{type}};
+    if ( $cfg->defined(q(type)) ) {
+        my @reports = @{$cfg->get(q(type));
         my @requested_types;
         foreach my $type ( @reports ) {
             if ( $report_types =~ /$type/ ) {
@@ -1848,26 +1924,26 @@ errors were encountered.
         }
         $report_types = join(q(:), @requested_types);
     }
-    if ( defined $opts{q(size-local)} ) {
-        if ( defined $opts{type} ) {
+    if ( $cfg->defined(q(size-local)) ) {
+        if ( $cfg->defined(q(type)) ) {
             $log->warn(qq(--size-local overrides any --type options used));
         }
         $report_types = q(size:local);
     }
-    if ( defined $opts{q(size-same)} ) {
-        if ( defined $opts{type} ) {
+    if ( $cfg->defined(q(size-same)) ) {
+        if ( $cfg->defined(q(type)) ) {
             $log->warn(qq(--size-same overrides any --type options used));
         }
         $report_types = q(size:same);
     }
 
     ### REPORT FORMATS
-    if ( defined $opts{format} ) {
-        $report_format = $opts{format};
+    if ( $cfg->defined(q(format)) ) {
+        $report_format = $cfg->get(format};
     }
 
-    if ( ! defined $opts{dotfiles} ) {
-        $opts{dotfiles} = 0;
+    if ( ! $cfg->defined(q(dotfiles)) ) {
+        $cfg->get(dotfiles} = 0;
     }
 
     my $stats = Runtime::Stats->new( report_format => $report_format );
@@ -1876,7 +1952,7 @@ errors were encountered.
     my $report = Reporter->new(
         report_format   => $report_format,
         report_types    => $report_types,
-        show_dotfiles   => $opts{dotfiles},
+        show_dotfiles   => $cfg->get(dotfiles},
     );
 
     # a list of files/directories were sync'ed with a mirror, either because
@@ -1894,9 +1970,9 @@ errors were encountered.
     if ( ! defined $dl_file ) {
         $log->logdie(qq(Could not download ls-laR.gz file));
     } else {
-        my $lslar_file = $opts{path} . q(/ls-laR.gz);
+        my $lslar_file = $cfg->get(q(path)) . q(/ls-laR.gz);
         $log->debug(qq(Set lslar_file to $lslar_file));
-        if ( defined $opts{sync} ) {
+        if ( $cfg->defined(q(sync)) ) {
             my $in_fh = IO::File->new(qq(< $lslar_file));
             my $file_digest;
             # create the digest object outside of any nested blocks
@@ -1929,8 +2005,8 @@ errors were encountered.
                 print qq(- Replacing file: $lslar_file\n);
                 print qq(- With file: $dl_file\n);
                 print qq(- Checksum mismatch, writing new ls-laR.gz file\n);
-                # FIXME add a check for $opts{sync} here prior to making the
-                # call to move()
+                # FIXME add a check for $cfg->get(q(sync)) here prior to making
+                # the call to move()
                 move($dl_file, $lslar_file);
             } else {
                 print qq( $lslar_file matches mirror copy\n);
@@ -1971,8 +2047,7 @@ errors were encountered.
             if ( $fields[PERMS] =~ /^-.*/ ) {
                 $total_archive_files++;
                 # skip this file if it's inside the /incoming directory
-                next if ( $incoming_dir_flag
-                    && ! defined $opts{incoming} );
+                next if ( $incoming_dir_flag && ! $cfg->defined(q(incoming)) );
                 my $archive_file = Archive::File->new(
                     parent          => $current_dir,
                     perms           => $fields[PERMS],
@@ -1986,18 +2061,18 @@ errors were encountered.
                 );
                 $total_archive_size += $archive_file->size;
                 my $local_file = Local::File->new(
-                    opts_path       => $opts{path},
+                    opts_path       => $cfg->get(path},
                     archive_obj    => $archive_file,
                 );
                 $report->write_record(
                     archive_obj    => $archive_file,
                     local_obj      => $local_file,
                 );
-                if ( defined $opts{sync} ) {
+                if ( $cfg->defined(q(sync)) ) {
                     if ( $local_file->needs_sync() ) {
                         if ( $local_file->sync(
                                 lwp             => $lwp,
-                                sync_dotfiles   => $opts{dotfiles} )
+                                sync_dotfiles   => $cfg->get(dotfiles} )
                         ) {
                             # add the file to the list of synced files
                             # used later on in reporting
@@ -2008,8 +2083,7 @@ errors were encountered.
             # the directory bit is set in the listing output
             } elsif ( $fields[PERMS] =~ /^d.*/ ) {
                 # skip this directory if it's inside the /incoming directory
-                next if ( $incoming_dir_flag
-                    && ! defined $opts{incoming} );
+                next if ( $incoming_dir_flag && ! $cfg->defined(q(incoming)) );
                 my $archive_dir = Archive::Directory->new(
                     parent          => $current_dir,
                     perms           => $fields[PERMS],
@@ -2023,25 +2097,26 @@ errors were encountered.
                     total_blocks    => 0,
                 );
                 my $local_dir = Local::Directory->new(
-                    opts_path       => $opts{path},
+                    opts_path       => $cfg->get(path},
                     archive_obj    => $archive_dir,
                 );
                 $report->write_record(
                     archive_obj    => $archive_dir,
                     local_obj      => $local_dir,
                 );
-                if ( defined $opts{sync} ) {
+                if ( $cfg->defined(q(sync)) ) {
                     if ( $local_dir->needs_sync() ) {
                         $local_dir->sync(
                             lwp             => $lwp,
-                            sync_dotfiles   => $opts{dotfiles},
+                            sync_dotfiles   => $cfg->get(dotfiles},
                         );
                     }
                 }
             # A new directory entry
             } elsif ( $fields[PERMS] =~ /^\.[\/\w\-_\.]*:$/ ) {
                 print qq(=== Entering directory: )
-                    . $fields[PERMS] . qq( ===\n) if ( defined $opts{headers} );
+                    . $fields[PERMS] . qq( ===\n)
+                    if ( $cfg->defined(q(headers)) );
                 # scrape out the directory name sans trailing colon
                 $current_dir = $fields[PERMS];
                 $current_dir =~ s/:$//;
@@ -2059,7 +2134,7 @@ errors were encountered.
                 # $1 got populated in the regex above
                 my $dir_blocks = $1;
                 print qq(- total blocks taken by this directory: $dir_blocks\n)
-                    if ( defined $opts{headers} );
+                    if ( $cfg->defined(q(headers)) );
             } elsif ( $line =~ /^lrwxrwxrwx.*/ ) {
                 print qq(- found a symlink: $current_dir\n)
                     if ( $log->is_info() );
